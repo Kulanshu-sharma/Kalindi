7d2586df85463d359b1751dbc419192ae3eca938
vaishnavi-diya15 <148624580+vaishnavi-diya15@users.noreply.github.com>
2023-10-26 21:16:48 +0530

    Login page connection with Databases

--- src/main/java/dbConnection/DBOperations.java
+++ src/main/java/dbConnection/DBOperations.java
@@ -2,6 +2,9 @@ package dbConnection;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Map;
 
 import dtos.ParamDTO;
 import dtos.ReplyDTO;
@@ -38,5 +41,53 @@ public class DBOperations {
 			replyDTO.setErrMsg("Database Connection error!!!");
 		}
 		return replyDTO;
+		
+	}
+
+	public static ReplyDTO Login(ParamDTO paramDTO) {
+		ReplyDTO replyDTO = new ReplyDTO();
+        String username=(String)paramDTO.getParams().get("userID");
+        String password=(String)paramDTO.getParams().get("password");
+        boolean authentic=false;
+        
+        
+        try {
+           
+            String sql = "SELECT * FROM users WHERE username = ? AND password = ?";
+            Connection con = DBConnection.getConnection();
+            PreparedStatement statement = con.prepareStatement(sql);
+            statement.setString(1, username);
+            statement.setString(2, password);
+            ResultSet resultSet = statement.executeQuery();
+            
+            if (resultSet.next()) {
+                System.out.println("Login successful!");
+                authentic=true;
+                
+            } else {
+                System.out.println("Invalid username or password.");
+                
+            }
+            try {
+            	if (authentic=true) {
+
+            		String query = "SELECT * FROM Student WHERE username = ?";
+            		PreparedStatement statement1 = con.prepareStatement(query);
+            		statement1.setString(1, username);
+            		statement1.executeQuery();
+            	}
+            }catch (SQLException e) {
+            	e.printStackTrace();
+            	
+            }
+
+          
+            resultSet.close();
+            statement.close();
+            con.close();
+        } catch (SQLException e) {
+        	e.printStackTrace();
+        }
+        return replyDTO;
 	}
 }
