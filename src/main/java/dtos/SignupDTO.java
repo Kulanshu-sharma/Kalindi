2698fadc78c7426873e8aaaab9928185d3ff6351
vaishnavi-diya15 <148624580+vaishnavi-diya15@users.noreply.github.com>
2023-10-30 19:25:53 +0530

    SignupDTO little changes

--- src/main/java/dtos/SignupDTO.java
+++ src/main/java/dtos/SignupDTO.java
@@ -7,6 +7,13 @@ public class SignupDTO {
 	private String lastName;
 	private String password;
 	private String course;
+	private String college_Rollno;
+	private String enrollment_No;
+	private String email_ID;
+	private String year;
+	private String semester;
+	private String DOB;
+	
 	
 	
 	public int getStudentId() {
@@ -38,6 +45,42 @@ public class SignupDTO {
 	}
 	public void setCourse(String course) {
 		this.course = course;
+	}
+	public String getCollege_Rollno() {
+		return college_Rollno;
+	}
+	public void setCollege_Rollno(String college_Rollno) {
+		this.college_Rollno = college_Rollno;
+	}
+	public String getEnrollment_No() {
+		return enrollment_No;
+	}
+	public void setEnrollment_No(String enrollment_No) {
+		this.enrollment_No = enrollment_No;
+	}
+	public String getEmail_ID() {
+		return email_ID;
+	}
+	public void setEmail_ID(String email_ID) {
+		this.email_ID = email_ID;
+	}
+	public String getYear() {
+		return year;
+	}
+	public void setYear(String year) {
+		this.year = year;
+	}
+	public String getSemester() {
+		return semester;
+	}
+	public void setSemester(String semester) {
+		this.semester = semester;
+	}
+	public String getDOB() {
+		return DOB;
+	}
+	public void setDOB(String dOB) {
+		DOB = dOB;
 	}
 	
 	
