4e4636dfeefe85c40c9da64af382f1145024cc73
kulanshusharma <kulanshusharma@192.168.29.100>
2023-11-22 22:52:40 +0530

    Responsive Logo Changes

--- src/main/java/bussinessLayer/StudentEnrollment.java
+++ src/main/java/bussinessLayer/StudentEnrollment.java
@@ -1,6 +1,10 @@
 package bussinessLayer;
 import java.util.List;
 import java.util.Scanner;
+
+import dbConnection.DBConnection;
+import dtos.ReplyDTO;
+
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -12,52 +16,45 @@ import java.sql.Connection;
 public class StudentEnrollment {
 
 
-	public static void main(String[] args) {
+	public static ReplyDTO studentElectives(int studentId) {
 
-		Scanner scanner = new Scanner(System.in);
+		ReplyDTO replyDTO = new ReplyDTO();
 		Connection connection = null;
-
-		try {
-		    Class.forName("com.mysql.cj.jdbc.Driver");
-
-		    connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/KALINDI_DB", "root", "#Kulanshu2011");
-
-		} catch (ClassNotFoundException | SQLException e) {
-		    e.printStackTrace();
-		}
-		
-		
-        System.out.print("Enter Student ID: ");
+        connection = DBConnection.getConnection();
+		        
+        String query = "SELECT s.SUBJECT_NAME AS SUBJECT_NAME FROM STUDENT_OTHER_INFO AS s1, SUBJECT_TBL as s WHERE s1.COLLEGE_ROLLNO=? AND s1.GE = s.SUBJECT_ID OR s1.AEC = s.SUBJECT_ID OR s1.VAC=s.SUBJECT_ID"
+        		+ " OR s1.SEC=s.SUBJECT_ID;";
         
-        int studentId = scanner.nextInt();
-        String query = "SELECT s.SUBJECT_NAME,s1.COLLEGE_ROLLNO  FROM STUDENT_OTHER_INFO AS s1, SUBJECT_TBL as s WHERE s1.COLLEGE_ROLLNO=? AND s1.GE = s.SUBJECT_ID OR s1.AEC = s.SUBJECT_ID OR s1.VAC=s.SUBJECT_ID\r\n"
-        		+ "OR s1.SEC=s.SUBJECT_ID;";
-        
-        try (PreparedStatement preparedStatement = connection.prepareStatement(query)) {
+        try{
+        	PreparedStatement preparedStatement = connection.prepareStatement(query);
             preparedStatement.setInt(1, studentId);
 
             ResultSet resultSet = preparedStatement.executeQuery();
 
-            List<String> enrolledSubjects = new ArrayList<>();
+            List<String> enrolledSubjects = new ArrayList<String>();
 
             while (resultSet.next()) {
-                String subjectId = resultSet.getString("subject_id");
-                String subjectName = resultSet.getString("subject_name");
-                enrolledSubjects.add("Subject ID: " + subjectId + ", Subject Name: " + subjectName);
+                String subjectName = resultSet.getString("SUBJECT_NAME");
+                enrolledSubjects.add(subjectName);
             }
 
             if (enrolledSubjects.isEmpty()) {
-                System.out.println("No subjects enrolled for the given student ID.");
+            	replyDTO.setErrFlag(true);
+                replyDTO.setErrMsg("No subjects enrolled for the given student ID.");
+                return replyDTO;
             } else {
-                System.out.println("Enrolled subjects for student ID " + studentId + ":");
-                for (String subject : enrolledSubjects) {
-                    System.out.println(subject);
-                }
+                replyDTO.setErrFlag(false);
+                replyDTO.setData(enrolledSubjects);
+                return replyDTO;
             }
 
         } catch (SQLException e) {
             e.printStackTrace();
+            replyDTO.setErrFlag(true);
+            replyDTO.setErrMsg("Internal Server Error");
         }
+        
+        return replyDTO;
     }
 
 }
--- src/main/java/dbConnection/DBOperations.java
+++ src/main/java/dbConnection/DBOperations.java
@@ -420,4 +420,46 @@ public class DBOperations {
 		}
 		return listOfSubjectIds;
 	}
+	
+	public static ReplyDTO studentElectives(int studentId) {
+
+		ReplyDTO replyDTO = new ReplyDTO();
+		Connection connection = null;
+        connection = DBConnection.getConnection();
+		        
+        String query = "SELECT s.SUBJECT_NAME AS SUBJECT_NAME FROM STUDENT_OTHER_INFO AS s1, SUBJECT_TBL as s WHERE s1.COLLEGE_ROLLNO=? AND s1.GE = s.SUBJECT_ID OR s1.AEC = s.SUBJECT_ID OR s1.VAC=s.SUBJECT_ID"
+        		+ " OR s1.SEC=s.SUBJECT_ID;";
+        
+        try{
+        	PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setInt(1, studentId);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            List<String> enrolledSubjects = new ArrayList<String>();
+
+            while (resultSet.next()) {
+                String subjectName = resultSet.getString("SUBJECT_NAME");
+                enrolledSubjects.add(subjectName);
+            }
+
+            if (enrolledSubjects.isEmpty()) {
+            	replyDTO.setErrFlag(true);
+                replyDTO.setErrMsg("No subjects enrolled for the given student ID.");
+                return replyDTO;
+            } else {
+                replyDTO.setErrFlag(false);
+                replyDTO.setData(enrolledSubjects);
+                return replyDTO;
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            replyDTO.setErrFlag(true);
+            replyDTO.setErrMsg("Internal Server Error");
+        }
+        
+        return replyDTO;
+    }
+
 }
